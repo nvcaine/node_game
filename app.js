e83164ba1c8ceb24cad8a72d0f4b28c73f666d3e
Romi Halasz <novocainex@gmail.com>
2016-05-05 00:09:43 +0300

    Actual source.

--- app.js
+++ app.js
@@ -1,23 +1,78 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var connectedClients = {};
+var targetPosition = {x:300, y:300};
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function() {
-	console.log('listening on *:3000');
+app.use('/assets', express.static(__dirname + '/assets'));
+
+http.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP, function() {
+//http.listen(8080, function() {
+	console.log('listening on: ' + process.env.OPENSHIFT_NODEJS_IP + ":" + process.env.OPENSHIFT_NODEJS_PORT);
 });
 
 io.on('connection', function(socket) {
-	console.log('a user connected');
+
+	var entityData = {x:50, y:50, destination_x:50, destination_y:50, score:0};
+	var clientIndex = socket.id
+	entityData.index = clientIndex;
+
+	console.log('------------------------------------------');
+	console.log('A user connected. Socket id: ' + socket.id);
+	console.log('------------------------------------------');
+
+	connectedClients[clientIndex] = entityData;
+
+	initConnection(socket, io, clientIndex);
+
+	io.to(socket.id).emit('server_initial_update', {clients:connectedClients, client_id:clientIndex, target:targetPosition}); // send all players info to current player, the new player is last
+	socket.broadcast.emit('server_update_new_player', entityData); // notify all others
+});
+
+function initConnection(socket, io, clientIndex) {
 
 	socket.on('disconnect', function() {
-		console.log('a user disconnected');
+
+		console.log('------------------------------------------');
+		console.log('A user disconnected; Socket id: ' + socket.id);
+		console.log('------------------------------------------');
+
+		delete connectedClients[clientIndex];
+
+		io.emit('server_update_disconnect_player', {index: clientIndex});
 	});
 
-	socket.on('client', function(clientData) {
-		io.emit('update', clientData);
+	socket.on('client_update', function(clientData) {
+
+		console.log('------------------------------------------');
+		console.log('Recevied client update (index ' + clientIndex + '):');
+		console.log(clientData);
+		console.log('------------------------------------------');
+
+		clientData.index = clientIndex;
+		connectedClients[clientIndex] = clientData;
+
+		io.emit('server_update', clientData);
 	});
-});
+
+	socket.on('client_score_update', function(clientData) {
+
+		console.log('------------------------------------------');
+		console.log('Recevied client score update (index ' + clientIndex + '):');
+		console.log('------------------------------------------');
+
+		connectedClients[clientIndex].score++;
+
+		targetPosition.x = Math.random() * 400 + 20;
+		targetPosition.y = Math.random() * 400 + 20;
+
+		io.emit('server_target_update', targetPosition);
+		io.emit('server_score_update', {index:clientIndex});
+	});
+}
